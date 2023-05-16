09a09636f842f5e44d66110666e9e146a8a8f21f
Ritika Rawat <Ritikarawat220@gmail.com>
2023-05-16 19:20:17 +0530

    Add home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './components/Home/Home';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </Router>
   );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+} from 'react-icons/ai';
+import { FaLinkedinIn } from 'react-icons/fa';
+import Type from './Type';
+import Avatar from '../../Assets/avatar.png';
+
+function Home() {
+  return (
+    <section>
+      <Container fluid className="home-section" id="home">
+        <Container className="home-content">
+          <Row>
+            <Col md={7} className="home-header">
+              <h1 style={{ paddingBottom: 15 }} className="heading">
+                Hi There!
+                {' '}
+                <span className="wave" role="img" aria-labelledby="wave">
+                  👋🏻
+                </span>
+              </h1>
+
+              <h1 className="heading-name">
+                I&apos;M
+                <strong className="main-name"> RITIKA RAWAT</strong>
+              </h1>
+
+              <div style={{ padding: 50, textAlign: 'left' }}>
+                <Type />
+              </div>
+            </Col>
+
+            <Col md={5} style={{ paddingBottom: 20 }}>
+              <img
+                src={Avatar}
+                alt="home pic"
+                className="img-fluid"
+                style={{ maxHeight: '450px' }}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col md={12} className="home-about-social">
+              <h1>FIND ME ON</h1>
+              <p>
+                Feel free to
+                {' '}
+                <span className="purple">connect </span>
+                with me
+              </p>
+              <ul className="home-about-social-links">
+                <li className="social-icons">
+                  <a
+                    href="https://github.com/ritikarawat220"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <AiFillGithub />
+                  </a>
+                </li>
+                <li className="social-icons">
+                  <a
+                    href="https://twitter.com/Ritikarawat22"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <AiOutlineTwitter />
+                  </a>
+                </li>
+                <li className="social-icons">
+                  <a
+                    href="https://www.linkedin.com/in/rawatritika/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <FaLinkedinIn />
+                  </a>
+                </li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    </section>
+  );
+}
+
+export default Home;
