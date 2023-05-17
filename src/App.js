6055a2c6c222114558b93635bac5aa403be99ac6
Ritika Rawat <Ritikarawat220@gmail.com>
2023-05-17 16:48:25 +0530

    Update styling

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Home from './components/Home/Home';
 import About from './components/About/About';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import ScrollToTop from './components/ScrollToTop';
 
 function App() {
-
   return (
     <Router>
       <div className="App">
