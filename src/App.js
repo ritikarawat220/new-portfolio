046ec50be9ed10f8c4c0bea5877ef3fe0815ff02
Ritika Rawat <Ritikarawat220@gmail.com>
2023-09-04 17:14:28 +0530

    Update styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
+import Tech from './components/Tech/tech';
 import Contact from './components/Contact/Contact';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -20,6 +21,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
+          <Route path="/tech" element={<Tech />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
